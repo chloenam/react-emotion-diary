8809de6fbbfc9062e7c068123c923816599a4e99
HyunJoo Nam <hvl2042@gepplus.co.kr>
2025-05-08 16:07:00 +0900

    feature: Home page action

--- src/App.jsx
+++ src/App.jsx
@@ -14,15 +14,21 @@ import { getEmotionImage } from "./util/get-emotion-images";
 const mockData = [
   {
     id: 1,
-    createdDate: new Date().getTime(),
+    createdDate: new Date("2025-05-08").getTime(),
     emotionId: 1,
     content: "1번 일기 내용",
   },
   {
     id: 2,
-    createdDate: new Date().getTime(),
+    createdDate: new Date("2025-05-05").getTime(),
     emotionId: 2,
     content: "2번 일기 내용",
+  },
+  {
+    id: 3,
+    createdDate: new Date("2025-04-16").getTime(),
+    emotionId: 3,
+    content: "3번 일기 내용",
   },
 ];
 
@@ -43,8 +49,8 @@ function reducer(state, action) {
   }
 }
 
-const DiaryStateContext = createContext();
-const DiaryDispatchContent = createContext();
+export const DiaryStateContext = createContext();
+export const DiaryDispatchContent = createContext();
 
 function App() {
   const [data, dispatch] = useReducer(reducer, mockData);
