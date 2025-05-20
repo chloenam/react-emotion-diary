98cb406590a9de4552c21f1578c8f387f3b5feee
HyunJoo Nam <hvl2042@gepplus.co.kr>
2025-05-20 14:56:31 +0900

    feature: New page action

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import { useNavigate } from "react-router-dom";
+import Button from "../components/Button";
+import Editor from "../components/Editor";
+import Header from "../components/Header";
+import { useContext } from "react";
+import { DiaryDispatchContent } from "../App";
 
 const New = () => {
-  return <div>new</div>;
+  const { onCreate } = useContext(DiaryDispatchContent);
+  const nav = useNavigate();
+
+  const onSubmit = (input) => {
+    onCreate(input.createdDate.getTime(), input.emotionId, input.content);
+    // replace : 뒤로가기 방지
+    nav("/", { replace: true });
+  };
+  return (
+    <div>
+      <Header
+        title={"새 일기 쓰기"}
+        leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로 가기"} />}
+      />
+      <Editor onSubmit={onSubmit} />
+    </div>
+  );
 };
 
 export default New;
